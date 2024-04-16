8a2271f6c951d49a4520594285562e218a45087b
Serena-Dong <serenadong3159@icloud.com>
2024-04-16 17:15:22 +0200

    header layout

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,39 +4,32 @@ import Jumbotron from './contents/Jumbotron.vue'
 <template>
     <header>
         <nav class="container-none flex justify-between">
-            <!-- Name -->
-            <div class="col">
-                <p class="m-0 font-bold">Serena</p>
-            </div>
 
             <!-- Functions -->
             <div class="col functions">
                 <ul class="flex flex-col gap-3  m-0">
-                    <li><a href="">about</a></li>
-                    <li><a href="">portfolio</a></li>
-                    <p class="m-0 font-bold"><i class="fa-regular fa-sun"></i> Light</p>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Works</a></li>
+                    <li><a href="">Contacts</a></li>
                 </ul>
             </div>
 
-
-            <!-- Light/Dark Mode
+            <!-- Light/Dark Mode -->
             <div class="col">
                 <p class="m-0 font-bold"><i class="fa-regular fa-sun"></i> Light</p>
-                <p class="m-0 font-bold"><i class="fa-solid fa-moon"></i> Dark</p>
-            </div> -->
+                <!-- <p class="m-0 font-bold"><i class="fa-solid fa-moon"></i> Dark</p> -->
+            </div>
         </nav>
 
         <!-- <jumbotron></jumbotron> -->
     </header>
 </template>
 <style lang="scss" scoped>
+@import "./../assets/scss/partials/variables";
+
 header {
+    padding: $mobile-padding;
+
     border: 1px solid black;
-
-    padding: 2rem;
-
-    nav {
-        margin-bottom: 3rem;
-    }
 }
 </style>
