9e72e9ee3944c7e237641ba97777f4dbc6ad74a2
Serena-Dong <serenadong3159@icloud.com>
2024-04-16 17:06:48 +0200

    demo commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -11,31 +11,29 @@ import Jumbotron from './contents/Jumbotron.vue'
 
             <!-- Functions -->
             <div class="col functions">
-                <ul class="flex gap-3  m-0">
+                <ul class="flex flex-col gap-3  m-0">
                     <li><a href="">about</a></li>
                     <li><a href="">portfolio</a></li>
-                    <li><a href="">contacts</a></li>
+                    <p class="m-0 font-bold"><i class="fa-regular fa-sun"></i> Light</p>
                 </ul>
             </div>
 
 
-            <!-- Light/Dark Mode -->
+            <!-- Light/Dark Mode
             <div class="col">
                 <p class="m-0 font-bold"><i class="fa-regular fa-sun"></i> Light</p>
                 <p class="m-0 font-bold"><i class="fa-solid fa-moon"></i> Dark</p>
-            </div>
+            </div> -->
         </nav>
 
-        <jumbotron></jumbotron>
+        <!-- <jumbotron></jumbotron> -->
     </header>
 </template>
 <style lang="scss" scoped>
 header {
     border: 1px solid black;
 
-    padding: 3rem;
-
-    width: 100vh;
+    padding: 2rem;
 
     nav {
         margin-bottom: 3rem;
